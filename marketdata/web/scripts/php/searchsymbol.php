a3badfe7db122ab229f8380e063f6c941d0ce922
tthaliath <tthaliath@gmail.com>
2017-09-08 14:40:24 -0700

    PHP scripts for login/register etc

---
+++ marketdata/web/scripts/php/searchsymbol.php
@@ -0,0 +1,23 @@
+<?php
+ $DBPWD = getenv('DBPASSWORD');
+ $con=mysqli_connect("ip-50-63-188-209.ip.secureserver.net","root",$DBPWD,"tickmaster");
+
+// Check connection
+ if (mysqli_connect_errno()) {
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+ $req = "SELECT a.ticker, a.comp_name "
+	."FROM tickermaster a "
+	."WHERE (a.comp_name LIKE '".$_REQUEST['term']."%' or a.ticker LIKE '".$_REQUEST['term']."%')"; 
+ $result = mysqli_query($con,$req);
+
+ while($row = mysqli_fetch_array($result)) {
+      $str = $row['ticker']."  ". $row['comp_name'];
+      $results[] = array('label' => $str);
+ }
+
+echo json_encode($results);
+ 
+ mysqli_close($con);
+ ?> 

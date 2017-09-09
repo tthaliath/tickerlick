6a78e774dda3df1a3bd91686c809c8cdfe07ed07
tthaliath <tthaliath@gmail.com>
2017-09-09 15:20:48 -0700

    test package to practise creation of package for distribution

---
+++ testpackages/testpackages/customer.py
@@ -0,0 +1,8 @@
+class customer:
+	def __init__(self,name='',balance=0):
+		self.name = name
+		self.balance = balance 
+	def getname(self):
+		return self.name
+	def getbalance(self):
+		return self.balance

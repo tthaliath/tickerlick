c5fd7694c076061f00cf3b9b3c2f7d4e0ad3c172
tthaliath <tthaliath@gmail.com>
2017-09-22 17:33:10 -0700

    Python package to download and process intraday quotes to generate buy/sell signals

--- tradevantage/setup.py
+++ tradevantage/setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
-long_description = 'generate security buy/sell signals using technical indicators RSI, Stochastic and Bollinger Band'
+long_description = 'generate security buy/sell signals using technical indicators like RSI, Stochastic and Bollinger Band'
 setup(name = 'tradevantage',
-	version = '1.0a',
-	description = 'generate security buy/sell signals using technical indicators RSI, Stochastic and Bollinger Band',
+	version = '1.1a',
+	description = 'generate security buy/sell signals using technical indicatorslike  RSI, Stochastic and Bollinger Band',
 	maintainer = 'Thomas Thaliath',
 	maintainer_email = 'tthaliath@gmail.com',
 	url = 'https://github.com/tthaliath/tickerlick/tree/master/tradevantage',

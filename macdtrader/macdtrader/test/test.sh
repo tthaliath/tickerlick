0976e20ae33c72a178ee80bb12cdde694afef446
tthaliath <tthaliath@gmail.com>
2017-10-04 18:40:38 -0700

    generate security buy/sell signals using technical indicator MACD

---
+++ macdtrader/macdtrader/test/test.sh
@@ -0,0 +1,9 @@
+# -t <list of stock symbols separated by comma. if not provided, will take symbols from symbolllist.txt>
+# -d date in yyyy-mm-dd format. default:today
+# -f symbol list file. default: symbollist.txt
+
+/usr/local/bin/python alphartq.py -t AAPL,SNAP,SRPT,AMD,NVDA,AMZN,GOOGL,TSLA,FB,NFLX,AGN,BABA,AVEO,BLK,FEYE,EFX,ORCL,SAGE
+
+#generate reports every two minutes. if email credentials are setup config.json file, report will be emailed
+
+/usr/local/bin/python reportquery.py

887cf3dd4a1871598b9c508049c6abef7d27f315
tthaliath <tthaliath@gmail.com>
2017-10-06 17:46:41 -0700

    Updated email alert format

--- tradevantage/tradevantage/reportquery.py
+++ tradevantage/tradevantage/reportquery.py
@@ -11,7 +11,6 @@ mylist = []
 today = str(datetime.date.today())
 mylist.append(today)
 today = mylist[0] # print the date object, not the container ;-)
-#today = '2017-09-19'
 json_data = json.load(open('config.json'))
 dbhost = json_data['mysql']['host']
 dbuser = json_data['mysql']['user']
@@ -21,21 +20,20 @@ tickmasterdb = json_data['mysql']['db']
 email_host =  json_data['email']['smtplibhost']
 email_to = json_data['email']['To']
 email_from = json_data['email']['From']
-def getrepquery(reptype):
-    return {
-        'R': "select 'RSI','Buy',d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.rsi_14 < 25  and d.rsi_14 != 0 union select 'RSI', 'Sell',d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.rsi_14 > 75  and d.rsi_14 != 0",
-	'S': "select 'Stochastic',  'Buy',d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.sma_3_3_stc_osci_14 < 20 and d.sma_3_3_stc_osci_14  >  1 union select 'Stochastic', 'Sell',d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.sma_3_3_stc_osci_14 > 80 and d.sma_3_3_stc_osci_14 < 99",
-	'B': "select 'Bollinger Band', 'Buy', d.ticker,d.rtq, d.rsi_14,abs((d.rtq - (d.dma_20 - (2 * d.dma_20_sd)))) 'bollidiff' from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker and d.rtq < (d.dma_20 - (2 * d.dma_20_sd)) union select 'Bollinger Band', 'Sell',d.ticker,d.rtq,d.rsi_14, abs((d.rtq - (d.dma_20 + (2 * d.dma_20_sd)))) 'bollidiff' from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker and d.rtq > abs((d.dma_20 + (2 * d.dma_20_sd)))"
-    }.get(reptype, 0)  
 
-query = getrepquery('R') + " union " + getrepquery('S') + " union " + getrepquery('B')
+def getrepquery(reptype):
+	return {
+	'R': "select indicator, tradesignal, ticker, rtq, rsi_14,sma_3_3_stc_osci_14 from (select 'RSI' as indicator,'Buy' as tradesignal ,d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.rsi_14 < 25  and d.rsi_14 != 0 union select 'RSI' as indicator, 'Sell'as tradesignal,d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.rsi_14 > 75  and d.rsi_14 != 0 union select 'Stochastic' as indicator,  'Buy' as tradesignal ,d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.sma_3_3_stc_osci_14 < 20 and d.sma_3_3_stc_osci_14  >  1 union select 'Stochastic' as indicator, 'Sell' as tradesignal,d.ticker,d.rtq,d.rsi_14,d.sma_3_3_stc_osci_14 from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker  and d.sma_3_3_stc_osci_14 > 80 and d.sma_3_3_stc_osci_14 < 99 union select 'Bollinger Band' as indicator, 'Buy' as tradesignal, d.ticker,d.rtq, d.rsi_14,abs((d.rtq - (d.dma_20 - (2 * d.dma_20_sd)))) 'bollidiff' from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker and d.rtq < (d.dma_20 - (2 * d.dma_20_sd)) union select 'Bollinger Band' as indicator, 'Sell' as tradesignal,d.ticker,d.rtq,d.rsi_14, abs((d.rtq - (d.dma_20 + (2 * d.dma_20_sd)))) 'bollidiff' from secpricert d,(select a.ticker,max(a.seq) maxseq from secpricert a where a.price_date = '"+today+"' group by a.ticker) b where d.seq = b.maxseq and d.ticker = b.ticker and d.rtq > abs((d.dma_20 + (2 * d.dma_20_sd)))) x order by x.ticker"
+ }.get(reptype, 0)
+
+query = getrepquery('R')
 file = open("lastreport.csv","w") 
 #print query
 try:
 	con = mdb.connect(dbhost,dbuser,dbpassword,tickmasterdb )
 	cur = con.cursor()
 	cur.execute(query)
-	file.write("indicator,ticker,signal,last price\n")
+	file.write("ticker,indicator,signal,last price\n")
 	for rec in cur:
 		indicator = rec[0]
 		signal = rec[1]
@@ -43,13 +41,13 @@ try:
 		rtq = rec[3]
 		rsi = rec[4]
 		stoch = rec[5]
-		line = indicator +","+ticker+","+signal+","+str(rtq) + "\n"
+		line = ticker +","+indicator+","+signal+","+str(rtq) + "\n"
 		file.write(line)
 	file.close()
 #check if email details have been setup. if so send a the report in a mail
 	if (email_host and email_to and email_from):
-#		print "email is setup"	
 		sendreport(email_from,email_to,email_host,'lastreport.csv')
+		print "Report has been sent to all recepients"
 except mdb.Error, e:
 
 	if con:
--- tradevantage/tradevantage/tradealert.py
+++ tradevantage/tradevantage/tradealert.py
@@ -5,11 +5,18 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+def getcolor(tradesignal):
+	if (tradesignal == 'Buy'):
+		return '#7FFF00'
+	else:
+		return '#DC143C'
+
+
 def sendreport(eFrom,eTo,eHost,MsgFile):
 	#print eFrom,eTo,eHost,MsgFile
 # Create message container - the correct MIME type is multipart/alternative.
 	msg = MIMEMultipart('alternative')
-	msg['Subject'] = "Tickerlick Trade Alert"
+	msg['Subject'] = "Tickerlick -  Trade Alert"
 	msg['From'] = eFrom 
 	msg['To'] = eTo 
 # Create the body of the message (HTML version).
@@ -18,12 +25,11 @@ def sendreport(eFrom,eTo,eHost,MsgFile):
 	f = open(MsgFile,"r")
 	for line in f.readlines():
 		fields = line.split(",")
-		hbody = hbody + "<tr><td>" + fields[0] + "</td><td>" + fields[1] + "</td><td>" + fields[2] + "</td><td>" + fields[3] + "</td></tr>"
+		hbody = hbody + "<tr><td>" + fields[0] + "</td><td>" + fields[1] + "</td><td bgcolor='"+getcolor(fields[2])+"'>" + fields[2] + "</td><td>" + fields[3] + "</td></tr>"
 	f.close()
-#	print hbody
 	html = """<html>
 	<head></head>
-	<body><p><br>
+	<body><p><br><table border="1">
 	"""
 	html = html + hbody 
   	html = html +  """<tr><td><a href="http://www.tickerlick.com">Visit TICKERLICK.COM!</a></td></tr>
@@ -31,7 +37,6 @@ def sendreport(eFrom,eTo,eHost,MsgFile):
    </br></p>
   </body>
 </html>"""
-#	print html
 # Record the MIME types text/html.
 	part2 = MIMEText(html, 'html')
 
